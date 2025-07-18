1d65391bc6861b3306f8f1a8e24df38942142a9e
Herculano-Quimuanga <herculanoquimuanga0@gmail.com>
2025-07-18 17:19:27 +0100

    Add bccryptjs for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mysql from "mysql2/promise";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs"; // Use bcryptjs para compatibilidade com Node.js 20+
 import jwt from "jsonwebtoken";
 import { GoogleGenAI } from "@google/genai";
 
