52f9e331181b4b7c307237f74e10944c7216b27c
Herculano Quimuanga <herculanoquimuanga0@gmail.com>
2025-08-13 13:01:27 +0100

    update path directory

--- server.js
+++ server.js
@@ -524,3 +524,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
